ced52609f3f0feea7aa1486a4b5c90e1b340b97b
unit-2_Program <100725778+mehfoozkhangithub@users.noreply.github.com>
2024-11-30 09:00:26 +0530

    code of movie app half and pending

---
+++ movie_api_app/src/App.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <div className="pt-16">
+        <Outlet />
+      </div>
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
