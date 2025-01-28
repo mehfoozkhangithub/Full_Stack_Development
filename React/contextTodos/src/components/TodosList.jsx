d201b8fd3d556b76e0a2e803b171c25cc8f287db
mehfoozkhangithub <khanmehfooz78603@gmail.com>
2025-01-28 20:26:39 +0530

    code of the todo list with edit functionallity working

--- React/contextTodos/src/components/TodosList.jsx
+++ React/contextTodos/src/components/TodosList.jsx
@@ -3,15 +3,18 @@ import { TodoContext } from "../Context/Context";
 
 export const TodosList = () => {
     const todoDataList = React.useContext(TodoContext);
-    const [toggelEdits, setToggelEdits] = React.useState(false)
+    const [toggelEdits, setToggelEdits] = React.useState(false);
+
+    const editHandel = (id) => {
+        todoDataList.editTodo(id);
+        setToggelEdits(true)
+    }
     return (<>
         <h1>Todo List</h1>
         {
             todoDataList.todo && todoDataList.todo.map((el, i) => (
                 <div key={i}>
                     <h1>{el.task}</h1>
-
-
                     {toggelEdits == true ?
                         <div>
                             <button onClick={() => { }}>done</button>
@@ -19,10 +22,9 @@ export const TodosList = () => {
                         </div>
                         :
                         <div>
-                            <button onClick={() => { todoDataList.editTodo(el.id) }}>edit</button>
+                            <button onClick={() => { editHandel(el.id) }}>edit</button>
                             <button onClick={() => { todoDataList.deleteTodo(el.id) }}>delete</button>
-                        </div>
-                    }
+                        </div>}
                 </div>
             ))
         }
