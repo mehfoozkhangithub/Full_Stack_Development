cc50a7481f985165d7095d51825eb768a2112c24
mehfoozkhangithub <khanmehfooz78603@gmail.com>
2025-02-02 17:10:41 +0530

    react -router

--- React/React_Router/src/Pages/Navbar.jsx
+++ React/React_Router/src/Pages/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export const Navbar = () => {
     return (<>
-        <div style={{ display: "flex", justifyContent: "space-around", alignItems: "center", width: "80%", margin: "auto", listStyle: "none" }}>
+        <div style={{ display: "flex", justifyContent: "space-around", alignItems: "center", width: "80%", margin: "auto", textDecoration: "none" }}>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/contact">Contact</Link>
