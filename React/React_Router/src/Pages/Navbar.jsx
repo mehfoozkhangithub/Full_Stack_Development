e653a5c11f08d508e21820d163bccf24819144db
mehfoozkhangithub <khanmehfooz78603@gmail.com>
2025-02-03 12:30:34 +0530

    code of the project

--- React/React_Router/src/Pages/Navbar.jsx
+++ React/React_Router/src/Pages/Navbar.jsx
@@ -1,12 +1,35 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export const Navbar = () => {
+
+    const links = [
+        { path: "/", title: "Home" },
+        { path: "/about", title: "About" },
+        { path: "/contact", title: "Contact" },
+        { path: "/login", title: "Login" }
+    ]
+
+
+
     return (<>
-        <div style={{ display: "flex", justifyContent: "space-around", alignItems: "center", width: "80%", margin: "auto", textDecoration: "none" }}>
+        {/* this without navlink conscept we have to optamise the code.  */}
+        {/* <div style={{ display: "flex", justifyContent: "space-around", alignItems: "center", width: "80%", margin: "auto", textDecoration: "none" }}>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/contact">Contact</Link>
             <Link to="/login">Login</Link>
-        </div>
+        </div> */}
+        {
+
+            links.map((data) => (
+                <div key={data.path} style={{ display: "flex", flexDirection: "row", justifyContent: "space-around", alignItems: "center", width: "80%", margin: "auto", textDecoration: "none" }}>
+                    <NavLink to={data.path} >
+                        {data.title}
+                    </NavLink>
+                </div>
+
+
+            ))
+        }
     </>)
 }
