cc0c6187ed5785e3e1bc49f8c9d5a3f113d35661
mehfoozkhangithub <khanmehfooz78603@gmail.com>
2025-02-06 11:28:53 +0530

    just delete extra space in code

--- React/React_Router/src/Components/Contact.jsx
+++ React/React_Router/src/Components/Contact.jsx
@@ -3,9 +3,7 @@ import { AuthContext } from "../Context/AuthContext";
 
 export const Contact = () => {
     const data = React.useContext(AuthContext);
-
     console.log(data);
-
     return (<>
         <h1>Contact</h1>
     </>)
