67d6b7d57268ad7f922888da19768d05a3410e07
mehfoozkhangithub <khanmehfooz78603@gmail.com>
2025-02-07 19:58:28 +0530

    code of the search param in react riuter dom

--- React/React_Router/src/Components/User.jsx
+++ React/React_Router/src/Components/User.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import { AuthContext } from './../Context/AuthContext';
 
 const getData = (url) => {
     return fetch(url).then((res) => res.json())
 }
 
+const getCurrentPageFromUrl = (value) => {
+    value = Number(value);
+    if (typeof value === "number" && value <= 0) value = 1;
+
+    if (!value) value = 1;
+
+    return value;
+}
+
 export const User = () => {
     const [data, setData] = React.useState({});
     const { toggelAuth } = React.useContext(AuthContext);
+    const [searchParam, setSearchParams] = useSearchParams();
+    const initial = getCurrentPageFromUrl(searchParam.get("page"))
+    const [page, setPage] = React.useState(initial);
     // const navigate = useNavigate();    // this code just to understand we sifted into PrivateRoute
     // const { isAuth } = React.useContext(AuthContext)  // this code just to understand we sifted into PrivateRoute
     // console.log(isAuth, "isAuth");
@@ -19,8 +31,13 @@ export const User = () => {
         //     navigate("/")
         // }
         // above code we have better approch private route component
-        getData(`https://reqres.in/api/users`).then((res) => setData(res))
-    }, []);
+        getData(`https://reqres.in/api/users?page=${page}`).then((res) => setData(res))
+    }, [page]);
+
+
+    React.useEffect(() => {
+        setSearchParams({ page })
+    }, [page])
 
     return (<>
         <h1>user</h1>
@@ -29,13 +46,16 @@ export const User = () => {
             {
                 data.data?.map((el, i) => (
                     <div key={i} style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "5px" }}>
-                        <img src={el.avatar} alt="" />
+                        <img src={el.avatar} alt="peron_logo" />
                         <Link to={`/users/${el.id}`}>
                             {el.first_name} {" "} {el.last_name}
                         </Link>
                     </div>
                 ))
             }
+            <button disabled={page === 1} onClick={() => setPage(page - 1)}>Prev</button>
+            <button>{page}</button>
+            <button disabled={page === 2} onClick={() => setPage(page + 1)}>Next</button>
         </div>
     </>)
 }
