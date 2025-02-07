a0b4eab56340bd5e0b6b87bf558354efeb68700a
mehfoozkhangithub <khanmehfooz78603@gmail.com>
2025-02-08 00:05:37 +0530

    just checking the code is pushing or not

--- React/React_Router/src/Components/Login.jsx
+++ React/React_Router/src/Components/Login.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 export const Login = () => {
-    return (<>
-        <h1>Login</h1>
-    </>)
-}
+  return (
+    <>
+      <h1>Login</h1>
+    </>
+  );
+};
+
+import React from "react";
--- React/React_Router/src/Context/AuthContext.jsx
+++ React/React_Router/src/Context/AuthContext.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
-
+// this is context create.
 export const AuthContext = React.createContext(null);
 
-
 export const AuthContextProvider = ({ children }) => {
-    const [isAuth, setIsAuth] = React.useState(false);
+  const [isAuth, setIsAuth] = React.useState(false);
 
-    const toggelAuth = () => {
-        setIsAuth(!isAuth)
-    }
-    return <AuthContext.Provider value={{ isAuth, toggelAuth }}>
-        {children}
+  const toggelAuth = () => {
+    setIsAuth(!isAuth);
+  };
+  return (
+    <AuthContext.Provider value={{ isAuth, toggelAuth }}>
+      {children}
     </AuthContext.Provider>
-}
+  );
+};
