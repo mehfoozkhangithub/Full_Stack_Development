490b1378d6d22c235604a243c0613ce79217e835
mehfoozkhangithub <khanmehfooz78603@gmail.com>
2025-02-09 13:57:20 +0530

    code

---
+++ React/useReducers/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Counters } from "./Components/Counters";
+
+function App() {
+  return (
+    <>
+      <Counters />
+    </>
+  );
+}
+
+export default App;
---
+++ React/useReducers/src/Components/Counters.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const inithislState = { count: 0 };
+const reducer = (state, action) => {
+  console.log(state.count);
+
+  if (action.type === "Incerement") {
+    return { count: state.count + 1 };
+  }
+  if (action.type === "Denerement") {
+    return { count: state.count - 1 };
+  }
+};
+
+export const Counters = () => {
+  const [state, dispatch] = React.useReducer(reducer, inithislState);
+  //  console.log(state.count);
+
+  return (
+    <>
+      <div>
+        <h1>Counter:{state.count}</h1>
+        <button onClick={() => dispatch({ type: "Incerement" })}>
+          Incerement
+        </button>
+        <button onClick={() => dispatch({ type: "Denerement" })}>
+          Decrement
+        </button>
+      </div>
+    </>
+  );
+};
