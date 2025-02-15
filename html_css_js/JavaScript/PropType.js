08a95f38defe893fa1c83735646bf4f2ce3dd141
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-02-15 21:17:55 +0530

    code of the math method

---
+++ html_css_js/JavaScript/PropType.js
@@ -0,0 +1,27 @@
+let number = 7;
+
+// let ans = Math.floor(number); // floor give u less number of the current value if 7.4 -> 7
+// let ans = Math.ceil(number); // ceil give u more value if 7.8 -> 8
+
+// let ans = Math.round(number); // basically work as a ceil
+
+/**
+ * ? this is we use to take square-root
+ *
+ * let ans = Math.sqrt(number);
+ *  ans = Math.ceil(ans);
+ *
+ * ? this is use to take a power of the number
+ *
+ * let ans = Math.pow(number);
+ *  ans = Math.ceil(ans);
+ *
+ *
+ */
+
+// let ans = Math.floor(Math.random() * 10); this is method took the random value or number between 0 to 10 number
+
+// let ans = Math.min(1, 6, 8, 3, 6); // this took the minimum value of the given number
+let ans = Math.max(1, 6, 8, 3, 6); // this will give u the max number of the given section
+
+console.log(ans);
