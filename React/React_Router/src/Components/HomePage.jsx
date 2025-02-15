64c22ad1b4d4aee694f324ac2a93dc4a95795f1a
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-02-15 12:23:36 +0530

    code test to push

--- React/React_Router/src/Components/HomePage.jsx
+++ React/React_Router/src/Components/HomePage.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { AuthContext } from './../Context/AuthContext';
-import { useNavigate } from 'react-router-dom';
+import { AuthContext } from "./../Context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
-    const { toggelAuth } = React.useContext(AuthContext)
-    const navigat = useNavigate()
-    if (toggelAuth) {
-        navigat("/users")
-    }
-    return (<>
-        <h1>Home</h1>
-        <button onClick={toggelAuth}>login</button>
-    </>)
-}
+  const { toggelAuth } = React.useContext(AuthContext);
+  const navigat = useNavigate();
+  if (toggelAuth) {
+    navigat("/users");
+  }
+  return (
+    <>
+      <h1>Home</h1>
+      <button onClick={toggelAuth}>login</button>
+    </>
+  );
+};
