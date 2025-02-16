758f250e5740cff30007c7d1769aee1dcdfcc4ae
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-02-16 11:57:19 +0530

    prototype code push

--- html_css_js/JavaScript/PropType.js
+++ html_css_js/JavaScript/PropType.js
@@ -29,3 +29,114 @@ let ans = Math.max(1, 6, 8, 3, 6); // this will give u the max number of the giv
 // ! PropType
 
 //? fact that is everything in js is object.
+
+// its is my blue-print
+let Products = {
+  brand: "nike",
+  seller: "amazon",
+};
+
+let prototypeProduct = Object.create(Products);
+prototypeProduct.name = "mehfooz";
+// console.log(prototypeProduct);
+
+let p1 = {
+  brand: "nike",
+  seller: "amazon",
+  price: 3000,
+};
+
+let p2 = {
+  brand: "nike",
+  seller: "amazon",
+  price: 3000,
+};
+
+let p3 = {
+  brand: "nike",
+  seller: "amazon",
+  price: 3000,
+};
+
+let p4 = {
+  brand: "nike",
+  seller: "amazon",
+  price: 3000,
+};
+
+// this is array we create by scratch
+
+let arr = new Array(1, 2, 3);
+// console.log(arr);
+
+// ! custom function or custom methods
+
+let Arr1 = [1, 2, 3];
+
+Array.prototype.धकेलो = function (value) {
+  let index = this.length;
+
+  this[index] = value;
+};
+
+Arr1.धकेलो(6);
+
+console.log(Arr1);
+/**
+ * 
+
+// Constructor function for Student
+function Student(name, age, marks) {
+  this.name = name;
+  this.age = age;
+  this.marks = marks; // Assume marks is an object {subject: marks}
+}
+
+// Prototype method to display student details
+Student.prototype.getDetails = function () {
+  console.log(`Student Name: ${this.name}`);
+  console.log(`Age: ${this.age}`);
+  console.log("Marks:", this.marks);
+};
+
+// Prototype method to update marks
+Student.prototype.updateMarks = function (subject, newMarks) {
+  if (this.marks[subject] !== undefined) {
+    this.marks[subject] = newMarks;
+    console.log(`Marks updated for ${subject}: ${newMarks}`);
+  } else {
+    console.log(`Subject ${subject} not found.`);
+  }
+};
+
+// Prototype method to check if the student passed (passing criteria: 40+ in all subjects)
+Student.prototype.isPassed = function () {
+  const failedSubjects = Object.keys(this.marks).filter(
+    (sub) => this.marks[sub] < 40
+  );
+
+  if (failedSubjects.length === 0) {
+    console.log(`${this.name} has passed all subjects! 🎉`);
+    return true;
+  } else {
+    console.log(`${this.name} failed in: ${failedSubjects.join(", ")} ❌`);
+    return false;
+  }
+};
+
+// Creating a student instance
+let student1 = new Student("John Doe", 18, {
+  Math: 85,
+  English: 78,
+  Science: 35,
+});
+
+// Using the prototype methods
+student1.getDetails(); // Display details
+student1.updateMarks("Science", 45); // Update marks
+student1.isPassed(); // Check pass/fail status
+
+
+ * 
+ * 
+ */
