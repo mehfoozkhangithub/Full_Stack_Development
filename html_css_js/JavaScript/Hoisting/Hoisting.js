ccd13386157735c1550d3af123bba0c1659350a4
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-02-20 17:06:07 +0530

    code of the hoisting with explaination

---
+++ html_css_js/JavaScript/Hoisting/Hoisting.js
@@ -0,0 +1,39 @@
+// this class is how js work
+
+//! what is hoisting?
+
+/**
+ * steps1 we have to go in python tutor
+ *
+ * --> inside this we have to check how hoisting work
+ *
+ * --> write block for the code
+ *
+ * var msg="hello"
+ *
+ * console.log(msg)
+ *
+ * 1. we get
+ *
+ *  msg=undefine
+ *
+ *  msg=hello
+ *
+ * * so we getting the value  after assign the var name or key-word
+ * *
+ *
+ *
+ * ? the we have to check the understand the position of assign
+ *
+ * console.log(msg)
+ *
+ * var msg;
+ *
+ * msg="hello"
+ *
+ * console.log(msg)
+ *
+ *
+ * todo :-  js raises the value on top and that is hoisting [legit attendance]. every variable `undefined` in the creation phase and hoisting is only for js not for other programming language
+ *
+ */
