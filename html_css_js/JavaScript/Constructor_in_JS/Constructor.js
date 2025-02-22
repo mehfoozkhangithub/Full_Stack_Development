daf9fb99e18ea617c67f08ba68df881f5179c01d
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-02-22 11:49:35 +0530

    Promis code done and constructo half done

--- html_css_js/JavaScript/Constructor_in_JS/Constructor.js
+++ html_css_js/JavaScript/Constructor_in_JS/Constructor.js
@@ -1,1 +1,12 @@
 // what is constructor.
+
+//! basically what 'new' keyword/operator does create instance of a user-defined object type of one of the built-in object type that has a constructor function
+
+// eg:- 👇
+
+function Car(make, model, year) {
+  (this.make = make), (this.model = model), (this.year = year);
+}
+const car1 = new Car("toyota", "toyota", 1995);
+
+console.log(car1.year);
--- html_css_js/JavaScript/Promise/Promise.js
+++ html_css_js/JavaScript/Promise/Promise.js
@@ -1,1 +1,49 @@
 // here we have to discuss the promises.
+
+// this promises 👇
+
+//? promise took callBack Function and also took (resolve,reject)
+
+/*?
+
+let ice_cream = new Promise((res, rej) => {
+  let got_ice_cream = true;
+  if (got_ice_cream) res(got_ice_cream);
+  else rej(got_ice_cream);
+}); // here i created new promises
+
+
+// we started the promises
+
+ice_cream.then(()=>{
+    console.log("eat ice cream");
+}).catch(()=>{
+    console.log("call mommy");
+})
+
+*/
+
+// what if we get delay to get the response
+
+let ice_cream = new Promise((res, rej) => {
+  let got_ice_cream;
+
+  setTimeout(() => {
+    got_ice_cream = true;
+    if (got_ice_cream) {
+      res("eat ice cream");
+    } else {
+      rej("call mom");
+    }
+  }, 3000);
+}); // here i created new promises
+
+// we started the promises
+
+ice_cream
+  .then(() => {
+    console.log("eat ice cream");
+  })
+  .catch(() => {
+    console.log("call mommy");
+  });
