97a3e931f27ff621ed1039d16649f43808707487
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-02-23 13:10:05 +0530

    code of axios

---
+++ React/axioReact/src/Components/ApiCall.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const getData = async (api) => {
+  /*
+  fetch(api)
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.log(err));
+  */
+  try {
+    let data = await fetch(api);
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const ApiCall = () => {
+  let url = "https://jsonplaceholder.typicode.com/todos";
+
+  React.useEffect(() => {
+    getData(url);
+  });
+  return (
+    <>
+      <h1>apiCall</h1>
+    </>
+  );
+};
