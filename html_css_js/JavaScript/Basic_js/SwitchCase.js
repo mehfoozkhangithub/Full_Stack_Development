16d0c688b56c7f464b724661b654ba17a1b9bb1d
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-02-25 12:14:19 +0530

    code of the data

---
+++ html_css_js/JavaScript/Basic_js/SwitchCase.js
@@ -0,0 +1,30 @@
+//basic
+
+let flag = true;
+
+// switch (true) {
+//   case true:
+//     console.log("true");
+//   case false:
+//     console.log("false");
+//   default:
+//     return 0;
+// }
+
+const cases = {
+  case_1: () => {
+    //logic
+  },
+  case_2: () => {
+    //logic
+  },
+  case_3: () => {
+    //logic
+  },
+};
+
+// let;
+
+//const
+
+// var
