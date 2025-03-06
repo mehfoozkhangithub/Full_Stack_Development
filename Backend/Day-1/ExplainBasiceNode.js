f38fdc2cb3f388033e54b2deb262d5869fd65db5
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-03-07 00:50:06 +0530

    code of the day

---
+++ Backend/Day-1/ExplainBasiceNode.js
@@ -0,0 +1,13 @@
+//! first question.
+
+// JS is
+
+console.log("a");
+
+// Day-2
+
+// how you interact with computer
+
+// 1-> GUI -> graphical user interface.
+
+// 2-> CMDLI -> Command Line Interface.
