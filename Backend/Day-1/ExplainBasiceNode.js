695cf9f7758e2adc1b5005607029801d3d0a0191
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-03-08 10:03:10 +0530

    code of the todo reducer useing hook

--- Backend/Day-1/ExplainBasiceNode.js
+++ Backend/Day-1/ExplainBasiceNode.js
@@ -3,11 +3,3 @@
 // JS is
 
 console.log("a");
-
-// Day-2
-
-// how you interact with computer
-
-// 1-> GUI -> graphical user interface.
-
-// 2-> CMDLI -> Command Line Interface.
---
+++ Backend/Day-2/evenValueNode.js
@@ -0,0 +1,7 @@
+// is-even.
+
+const checkEven = require("is-even");
+
+let ans = checkEven(5);
+
+console.log(ans);
