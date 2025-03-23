738e8d441aa8fb43f1406a3cef5b5d593b407eb9
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-03-23 16:26:10 +0530

    create dir for upcomming days to store the data

--- Backend/Day-4/index2.js
+++ Backend/Day-4/index2.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const fs = require("fs");
+const { stringify } = require("querystring");
 const Port = 8000;
 
 const app = express();
 
 // crud operation...
 
-app.get("./", (req, res) => {
+app.get("/", (req, res) => {
   res.send("add data...");
+});
+
+app.post("/students", (req, res) => {
+  fs.writeFileSync("addStudents", JSON.stringify(req.body), "utf-8");
+  res.send("accepted data...");
 });
 
 app.listen(Port, () => {
---
+++ Backend/Day-6/MongoDb.md
@@ -0,0 +1,9 @@
+# What is MongoDb?
+
+- NO-SQL/Non Relational Database
+- High Performance
+- Highly Scalable
+- map/reducer Aggragations
+- Auto Sharding
+
+# Why
