d7f77d5499e0fd0fc4436659a99dde468f68beed
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-04-02 06:56:17 +0530

    prototype

--- html_css_js/JavaScript/Basic_js/Prototype/Prototype.js
+++ html_css_js/JavaScript/Basic_js/Prototype/Prototype.js
@@ -1,5 +1,7 @@
 let arr = [1, 2, 3, 4];
 
+// push
+
 // Array.prototype.धकेलो = function (value) {
 //   let index = this.length;
 //   this[index] = value;
@@ -7,6 +9,8 @@ let arr = [1, 2, 3, 4];
 
 // arr.धकेलो(7);
 // console.log(arr);
+
+// pop
 
 Array.prototype.निकालो = function () {
   let index = this.length - 1;
@@ -17,3 +21,99 @@ Array.prototype.निकालो = function () {
 arr.निकालो();
 
 console.log(arr);
+
+// shift
+
+Array.prototype.निकालो_शुरुआत = function () {
+  if (this.length === 0) return undefined;
+
+  let firstElement = this[0];
+
+  // Shift elements to the left
+  for (let i = 0; i < this.length - 1; i++) {
+    this[i] = this[i + 1];
+  }
+
+  delete this[this.length - 1];
+  this.length -= 1;
+
+  return firstElement;
+};
+
+// Example Usage:
+let arr3 = [10, 20, 30, 40];
+let removedElement = arr3.निकालो_शुरुआत();
+console.log(removedElement); // 10
+console.log(arr3); // [20, 30, 40]
+
+// unshift
+
+Array.prototype.आगे_डालो = function (...values) {
+  let newLength = this.length + values.length;
+
+  // Shift existing elements to the right
+  for (let i = newLength - 1; i >= values.length; i--) {
+    this[i] = this[i - values.length];
+  }
+
+  // Add new elements at the beginning
+  for (let i = 0; i < values.length; i++) {
+    this[i] = values[i];
+  }
+
+  return this.length;
+};
+
+// Example Usage:
+let arr2 = [3, 4, 5];
+arr2.आगे_डालो(1, 2);
+console.log(arr2); // [1, 2, 3, 4, 5]
+
+// forEach
+/*
+Array.prototype.प्रत्येक = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (i in this) {
+      callback(this[i], i, this);
+    }
+  }
+};
+
+// Example Usage:
+let arr = [1, 2, 3, 4];
+arr.प्रत्येक((num, index) => {
+  console.log(`Index: ${index}, Value: ${num}`);
+});
+*/
+
+// map
+/*
+Array.prototype.नक्शा = function (callback) {
+  let newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (i in this) {
+      newArray[i] = callback(this[i], i, this);
+    }
+  }
+  return newArray;
+};
+
+let arr = [1, 2, 3, 4];
+let result = arr.नक्शा((num) => num * 2);
+console.log(result); // [2, 4, 6, 8]
+*/
+
+// filter
+
+Array.prototype.छानना = function (callback) {
+  let newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (i in this && callback(this[i], i, this)) {
+      newArray.push(this[i]);
+    }
+  }
+  return newArray;
+};
+
+let filteredArr = arr.छानना((num) => num % 2 === 0);
+console.log(filteredArr); // [2, 4]
