28eab6068ace1b71db12f400701ad3910a01ed5d
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-04-02 06:47:44 +0530

    code of the day

--- Backend/Day-6/index.js
+++ Backend/Day-6/index.js
@@ -2,6 +2,25 @@ const express = require("express");
 const Port = 8900;
 const app = express();
 
+const { routersData } = require("./Routes/dataAdd.routes");
+
+app.use(express.json());
+
+app.use("/addData", routersData);
+
+app.get("/", (req, res) => {
+  res.send("welcom");
+});
+
+app.get("/about", (req, res) => {
+  res.send("about");
+});
+
+app.post("/addData", (req, res) => {
+  console.log(req.body);
+  res.send("added data form db...");
+});
+
 app.listen(Port, () => {
   console.log(`your are running on ${Port}`);
 });
