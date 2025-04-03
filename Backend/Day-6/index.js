84cfd24668c0d311c9eb50516ac97d786bc1e676
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-04-03 15:59:24 +0530

    code of the dataBase and param or query concept clear

--- Backend/Day-6/index.js
+++ Backend/Day-6/index.js
@@ -2,19 +2,42 @@ const express = require("express");
 const Port = 8900;
 const app = express();
 
-const { routersData } = require("./Routes/dataAdd.routes");
+const { dataAddedRouter } = require("./Routes/dataAdd.routes");
 
 app.use(express.json());
 
-app.use("/addData", routersData);
+app.use("/data", dataAddedRouter);
 
 app.get("/", (req, res) => {
   res.send("welcom");
 });
 
 app.get("/about", (req, res) => {
-  res.send("about");
+  const { course } = req.query;
+  res.send(`this is the dynimic param ${course}`);
 });
+
+// concept of query explain below
+
+app.get("/weather", (req, res) => {
+  const data = {
+    bangalore: "winter",
+    california: "spring",
+    londan: "summery",
+  };
+  const city = req.query.city;
+  const weather = data[city];
+  res.send(`Today weather in ${city} is ${weather}`);
+});
+
+// here we have to implement params
+
+app.get("/students/:studentId", (req, res) => {
+  const ID = req.params.studentId;
+  res.send(`Here is the data od student who's ID is ${ID}`);
+});
+
+// video -> 57:33
 
 app.post("/addData", (req, res) => {
   console.log(req.body);
