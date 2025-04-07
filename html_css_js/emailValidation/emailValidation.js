73689b33880e0966666c81ab30608091c8e2e792
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-04-07 15:36:13 +0530

    code of the day

--- html_css_js/emailValidation/emailValidation.js
+++ html_css_js/emailValidation/emailValidation.js
@@ -24,8 +24,24 @@ function myFunctions() {
       console.log(`please enter the value...`);
     } else if (pass.length > 8 && pass.length > 20) {
       console.log(`please enter the correct length...`);
+    } else {
+      const hasUpperCase = /[A-Z]/.test(pass);
+      const hasLowerCase = /[a-z]/.test(pass);
+      const hasNumber = /[0-9]/.test(pass);
+      const hasSpecialChar = /[!@#$%^&*(),.?\":{}|<>]/.test(pass);
+
+      if (!hasUpperCase) {
+        console.log("Password must inclued at least one UpperCase Letter...");
+      } else if (!hasLowerCase) {
+        console.log("Password must inclued at least one LowerCase Letter...");
+      } else if (!hasNumber) {
+        console.log("Password must inclued at least one Number...");
+      } else if (!hasSpecialChar) {
+        console.log("Password must inclued at least one Special Character...");
+      } else {
+        console.log("Password is Valid...");
+      }
     }
-    // else if(){}
   }
 }
 
