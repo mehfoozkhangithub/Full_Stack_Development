f6faa7b52e0becda5138bef19ca935812614609b
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-04-13 21:50:00 +0530

    code of the day

--- Project/Weather/WeatherApp.js
+++ Project/Weather/WeatherApp.js
@@ -1,23 +1,30 @@
+const api_key = "AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8";
+
 const apiCall = async () => {
   const api_key = `5881c4a70f1f474bc5289105d70aa1b5`;
-  let city = document.getElementById("city").value;
-  const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+  let citys = document.getElementById("city").value;
+  const api = `https://api.openweathermap.org/data/2.5/weather?q=${citys}&appid=${api_key}`;
 
-  if (city.trim() === "") return;
+  if (citys.trim() === "") return;
 
   try {
     let res = await fetch(api);
     let data = await res.json();
-    appends(data);
+    appends(data, citys);
   } catch (err) {
     console.log(err);
   }
 };
 
-const appends = (value) => {
+const appends = (value, citys) => {
   let main = document.getElementById("container");
 
-  console.log(value);
+  let iframe = document.getElementById("iframe_tag");
+
+  let api = `https://www.google.com/maps/embed/v1/place?q=${citys}&key=${api_key}`;
+
+  iframe.src = api;
+
   let div = document.createElement("div");
 
   let name = document.createElement("h1");
@@ -54,6 +61,6 @@ const appends = (value) => {
   main.append(div);
 };
 
-let number = 3.4;
+// let number = 3.4;
 
-console.log(Math.trunc(number));
+// console.log(Math.trunc(number));
