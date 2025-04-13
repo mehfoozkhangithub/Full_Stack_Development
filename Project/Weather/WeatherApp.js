7d872e11c7c8cba2db1f6a40b81cf0738a66b8b5
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-04-13 15:14:15 +0530

    code of the day weather

--- Project/Weather/WeatherApp.js
+++ Project/Weather/WeatherApp.js
@@ -30,13 +30,23 @@ const appends = (value) => {
 
   // formula -> (300°F − 32) × 5/9 = 148.889°C
 
-  temp.innerText = `temp : ${value.main.temp}`;
+  let new_temp = Math.trunc(value.main.temp);
+  let convert_to_degree_temp = Math.trunc(((new_temp - 32) * 5) / 9);
+
+  temp.innerText = `temp : ${convert_to_degree_temp}°C`;
 
   let temp_min = document.createElement("p");
-  temp_min.innerText = `temp_min : ${value.main.temp_min}`;
+  let new_min_temp = Math.trunc(value.main.temp_min);
+  let convert_to_degree_min_temp = Math.trunc(((new_min_temp - 32) * 5) / 9);
+
+  temp_min.innerText = `temp_min : ${convert_to_degree_min_temp}°C`;
 
   let temp_max = document.createElement("p");
-  temp_max.innerText = `temp_max : ${value.main.temp_max}`;
+  let new_max_temp = Math.trunc(value.main.temp_max);
+  let convert_to_degree_max_temp = Math.trunc(((new_max_temp - 32) * 5) / 9);
+  console.log(convert_to_degree_max_temp);
+  console.log(convert_to_degree_min_temp);
+  temp_max.innerText = `temp_max : ${convert_to_degree_max_temp}°C`;
   //   let p = document.createElement("p");
   //   let p = document.createElement("p");
 
@@ -46,5 +56,4 @@ const appends = (value) => {
 
 let number = 3.4;
 
-number.ceil();
-console.log(number);
+console.log(Math.trunc(number));
