ff008e541d3c61a2b4698635348861aa87fad84a
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-04-15 15:54:35 +0530

    code of the day

--- Backend/Day-8/index.js
+++ Backend/Day-8/index.js
@@ -15,18 +15,24 @@ app.get("/user", async (req, res) => {
     res.send(users);
   } catch (err) {
     console.log(err);
+    res.send({ err: "something went wrong..." });
   }
 });
 app.post("/createuser", async (req, res) => {
-  let payload = req.body;
-  //*  this is the way we insert data in mongo db formate
-  // await UserModule.insertMany([payload]);
+  try {
+    let payload = req.body;
+    //*  this is the way we insert data in mongo db formate
+    // await UserModule.insertMany([payload]);
 
-  //? now we insert the data in mongoose way
-  // in case if you inserting in one data use this code write below
-  const payloadMongooseWay = new UserModule(payload);
-  await payloadMongooseWay.save();
-  res.send("sucessfully created");
+    //? now we insert the data in mongoose way
+    // in case if you inserting in one data use this code write below
+    const payloadMongooseWay = new UserModule(payload);
+    await payloadMongooseWay.save();
+    res.send("sucessfully created");
+  } catch (error) {
+    console.log(err);
+    res.send({ err: "something went wrong..." });
+  }
 });
 
 app.listen(port, async () => {
