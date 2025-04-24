b12279291a528b46c0b98b148a73ec34936fa576
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-04-25 01:27:11 +0530

    code of the day

--- Backend/Day-11/Server.js
+++ Backend/Day-11/Server.js
@@ -2,10 +2,20 @@ const express = require("express");
 
 require("dotenv").config();
 
+const { Connections } = require("./config/db");
+
 const app = express();
 
 app.use(express.json());
 
-app.listen(process.env.Port, () => {
+app.listen(process.env.Port, async () => {
+  try {
+    await Connections;
+    console.log("Connected to DB Succesfully...");
+  } catch (err) {
+    console.log("Connected to DB Failed??");
+    console.log(err);
+  }
   console.log(`Server ws connected to ${process.env.Port}`);
 });
+// js
