9991312b763bf817d6f576b88cc526d55f35d7e5
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-04-25 12:52:09 +0530

    code of the data

--- Backend/Day-11/Server.js
+++ Backend/Day-11/Server.js
@@ -1,12 +1,64 @@
 const express = require("express");
-
 require("dotenv").config();
 
 const { Connections } = require("./config/db");
+const { UserModule } = require("./model/User.model");
 
 const app = express();
 
 app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const payload = req.body;
+  // console.log(payload);
+  try {
+    const users = new UserModule(payload);
+    await users.save();
+    res.send("Signup Succesfull...");
+  } catch (err) {
+    console.log(err);
+    res.send("something went wrong! please try again leater...");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const checkUserDetails = await UserModule.find({ email, password }); // here we pass the email/pas into the find is useing and-operator
+    if (checkUserDetails.length > 0) {
+      res.send("login Succesfull...");
+    } else {
+      res.send("login Unsuccesfull...");
+    }
+  } catch (err) {
+    console.log(err);
+    res.send("something went wrong! please try again leater...");
+  }
+});
+
+app.get(
+  "/about",
+  (req,
+  (res) => {
+    res.send("this is about...");
+  })
+);
+
+app.get(
+  "/weather",
+  (req,
+  (res) => {
+    res.send("this is weather...");
+  })
+);
+
+app.get(
+  "/purchased",
+  (req,
+  (res) => {
+    res.send("your purchaseh token...");
+  })
+);
 
 app.listen(process.env.Port, async () => {
   try {
