2bf28aac49a5f4f72efcb32ff520c159c574c799
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-04-28 17:26:33 +0530

    code of the day authentication

--- Backend/Day-11/Server.js
+++ Backend/Day-11/Server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 const { Connections } = require("./config/db");
 const { UserModule } = require("./model/User.model");
@@ -7,6 +8,10 @@ const { UserModule } = require("./model/User.model");
 const app = express();
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("welcome...");
+});
 
 app.post("/signup", async (req, res) => {
   const payload = req.body;
@@ -26,7 +31,8 @@ app.post("/login", async (req, res) => {
   try {
     const checkUserDetails = await UserModule.find({ email, password }); // here we pass the email/pas into the find is useing and-operator
     if (checkUserDetails.length > 0) {
-      res.send("login Succesfull...");
+      const token = jwt.sign({ course: "nxm" }, "hush");
+      res.send({ msg: "login Succesfull...", token: token });
     } else {
       res.send("login Unsuccesfull...");
     }
@@ -36,29 +42,34 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.get(
-  "/about",
-  (req,
-  (res) => {
-    res.send("this is about...");
-  })
-);
+app.get("/about", (req, res) => {
+  res.send("this is about...");
+});
 
-app.get(
-  "/weather",
-  (req,
-  (res) => {
-    res.send("this is weather...");
-  })
-);
+app.get("/weather", (req, res) => {
+  const token = req.query.token;
+  // const token - req.headers.authoriztion?.split(" ")[1]; //* this we have to put and the-> correct manner is this
+  const decoded = jwt.verify(token, "hush", (err, decoded) => {
+    if (err) {
+      res.send("please login first!!!");
+    } else if (decoded) {
+      res.send("this is weather...");
+    }
+  });
+});
 
-app.get(
-  "/purchased",
-  (req,
-  (res) => {
-    res.send("your purchaseh token...");
-  })
-);
+app.get("/purchased", (req, res) => {
+  const token = req.query;
+
+  console.log(token);
+  if (token.token === "abc123") res.send("your purchaseh token...");
+
+  res.send("please login first!!!");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("this is contact...");
+});
 
 app.listen(process.env.Port, async () => {
   try {
