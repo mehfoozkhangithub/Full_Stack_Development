d9f69738390070a60e5d63b1bf3e67aa6c089d1f
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-05-05 19:48:16 +0530

    code of the day

--- html_css_js/JavaScript/Clouser/Clouser.js
+++ html_css_js/JavaScript/Clouser/Clouser.js
@@ -1,3 +1,24 @@
 // clouser topics
 
 // clouseris related to function...
+
+// this is the parent function
+const justFunction = () => {
+  let n = "mehfooz"; // this goig to garbage
+
+  return () => {
+    // this is the child function of the parent function
+    console.log(n);
+  };
+};
+
+// this is the one way of the calling the function
+console.log(justFunction()());
+
+// this is the one way to invoke the functions
+
+/*
+const ans = justFunction();
+
+ans();
+*/
