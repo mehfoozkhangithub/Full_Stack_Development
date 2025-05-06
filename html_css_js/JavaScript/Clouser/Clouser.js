99968cd76cbb7f862206b69d23f672a2c3188061
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-05-06 18:46:08 +0530

    code of the day

--- html_css_js/JavaScript/Clouser/Clouser.js
+++ html_css_js/JavaScript/Clouser/Clouser.js
@@ -7,8 +7,9 @@ const justFunction = () => {
   let n = "mehfooz"; // this goig to garbage
 
   return () => {
+    let n = "hello"; // because this is running and prev n is destroyed after end of exicutional stack...
     // this is the child function of the parent function
-    console.log(n);
+    return n;
   };
 };
 
@@ -21,4 +22,14 @@ console.log(justFunction()());
 const ans = justFunction();
 
 ans();
+*/
+
+/*   
+
+notes :- it's closes the variable that are depedence it's like frezzes them it's dosent allow them to destroyed only there is some dependence, and what that means of dependence  was -> A child function need a variable  from a parent function even those parent function had been called or after the function called the varaiable destroyed but the parent func share the  variable to the chile function...
+
+
+
+closure happend to nested function or there has to any depedence to parent func to child fucn...
+
 */
