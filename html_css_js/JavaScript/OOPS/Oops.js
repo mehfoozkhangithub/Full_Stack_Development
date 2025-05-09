a87a136100c141ba90cea37bb194846cdbbf3346
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-05-09 12:28:27 +0530

    code of the day

--- html_css_js/JavaScript/OOPS/Oops.js
+++ html_css_js/JavaScript/OOPS/Oops.js
@@ -160,3 +160,54 @@ let v2 = new Car1();
 let v3 = new Truck();
 
 // v1.run();
+
+// this is different example of polymorphism
+
+class Animal {
+  speak() {
+    console.log("Animal speaks");
+  }
+}
+
+class Dog extends Animal {
+  speak() {
+    console.log("Dog barks");
+  }
+}
+
+class Cat extends Animal {
+  speak() {
+    console.log("Cat meows");
+  }
+}
+
+function makeSound(animal) {
+  animal.speak(); // Polymorphic behavior
+}
+
+let dog = new Dog();
+let cat = new Cat();
+
+makeSound(dog); // Dog barks
+makeSound(cat); // Cat meows
+
+// this is with object no class method
+
+const printer = {
+  print: () => console.log("Default print"),
+};
+
+const pdfPrinter = {
+  print: () => console.log("Printing PDF"),
+};
+
+const htmlPrinter = {
+  print: () => console.log("Printing HTML"),
+};
+
+function startPrint(printerObj) {
+  printerObj.print();
+}
+
+startPrint(pdfPrinter); // Printing PDF
+startPrint(htmlPrinter); // Printing HTML
