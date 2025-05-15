400f7294265eb1a8e6642ae0549557282f75a70f
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-05-15 13:20:02 +0530

    code of the day

---
+++ Project/Pagination_js/index.js
@@ -0,0 +1,46 @@
+let pages;
+let api = `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pages}`;
+
+const appendData = (value) => {
+  const main = document.querySelector(".main");
+
+  value.forEach((el) => {
+    let container = document.createElement("div");
+    let id = document.createElement("p");
+    let text = document.createElement("p");
+
+    container.classList.add("container");
+
+    id.innerText = el.id;
+    id.classList.add("id_data");
+    text.innerText = el.title;
+
+    container.append(id, text);
+    main.append(container);
+  });
+};
+
+const dataFetch = async () => {
+  try {
+    let res = await fetch(api);
+    let data = await res.json();
+    appendData(data);
+
+    // console.log(" data:", data);
+  } catch (error) {
+    console.log(" error:", error);
+  }
+};
+
+let prev = document.querySelector("#prev");
+let next = document.querySelector("#next");
+
+const prevBtnInvokation = () => {
+  pages--;
+  console.log(" pages:", pages);
+};
+
+const nextBtnInvokation = () => {
+  pages++;
+  console.log(" pages:", pages);
+};
