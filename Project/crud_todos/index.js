196a4d57a8b456ae94a7d3639fb1e95b931b8246
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-05-22 11:44:38 +0530

    code of the day build the todo app with html and css

---
+++ Project/crud_todos/index.js
@@ -0,0 +1,39 @@
+let arrStorage = [];
+
+let localStorages = JSON.parse(localStorage.getItem("todos") || []);
+
+const myTodos = () => {
+  const todoVal = document.querySelector("#todos").value;
+  if (todoVal.length === 0) {
+    return;
+  }
+  let data = {
+    id: Math.random().toString(36).substring(2, 15),
+    todoText: todoVal,
+    isCompleted: false,
+  };
+
+  arrStorage.push(data);
+  localStorage.setItem("todos", JSON.stringify(arrStorage));
+  console.log("your data has been add in to ls...");
+  document.querySelector("#todos").value = "";
+  appendData();
+};
+
+const appendData = () => {
+  const dataInfo = document.querySelector("#dataInfo");
+
+  localStorages.map((el, i) => {
+    let div = document.createElement("div");
+    let id = document.createElement("p");
+    let text = document.createElement("p");
+
+    div.className = "dataRow";
+
+    id.innerText = el.id;
+    text.innerText = el.todoText;
+
+    div.append(id, text);
+    dataInfo.append(div);
+  });
+};
