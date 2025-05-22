643490a0fcb29fcb267dbfa8b2a55bcbf669f1d7
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-05-22 20:29:28 +0530

    code of the day -> lazy loading the data on network call

--- html_css_js/JavaScript/lazy_loading/index.js
+++ html_css_js/JavaScript/lazy_loading/index.js
@@ -9,3 +9,23 @@ https://www.youtube.com/watch?v=Qhn92q5lIbs
 https://www.youtube.com/watch?v=v_sMvx-CMxs
 
 */
+
+function loadScript(url) {
+  let isLoaded = document.querySelector(".dynamic-script");
+
+  if (isLoaded.length > 0) {
+    console.log("not loading the scripts");
+    return;
+  }
+  let myScript = document.createElement("script");
+  myScript.src = url;
+  myScript.className = "dynamic-script";
+  document.body.append(myScript);
+  console.log("loading the scripts");
+}
+
+let searchInput = document.querySelector("#search_input");
+
+searchInput.addEventListener("focus", function (e) {
+  loadScript("https://code.jquery.com/jquery-3.6.1.min.js");
+});
