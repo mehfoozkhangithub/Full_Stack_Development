a3d0790ecb91de45882b3a8d9974d568aea3f1be
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-05-24 12:28:30 +0530

    code of the day

--- Project/crud_todos/index.js
+++ Project/crud_todos/index.js
@@ -1,8 +1,7 @@
-let arrStorage = [];
+let arrStorage = JSON.parse(localStorage.getItem('todos')) || [];
 
-let localStorages = JSON.parse(localStorage.getItem("todos")) || [];
 const myTodos = () => {
-  const todoVal = document.querySelector("#todos").value;
+  const todoVal = document.querySelector('#todos').value;
   if (todoVal.length === 0) {
     /*  todoVal.innerHTML =
       "<p>You have empty todo text.. please fill the text for todos</p>"; */
@@ -16,25 +15,39 @@ const myTodos = () => {
   };
 
   arrStorage.push(data);
-  localStorage.setItem("todos", JSON.stringify(arrStorage));
-  console.log("your data has been add in to ls...");
-  document.querySelector("#todos").value = "";
-  appendData(localStorages);
+  console.log(arrStorage, 'this is befor storing in the ls');
+  localStorage.setItem('todos', JSON.stringify(arrStorage));
+  appendData();
+  console.log('your data has been add in to ls...');
+  document.querySelector('#todos').value = '';
 };
 
-const appendData = (localStorages) => {
-  const dataInfo = document.querySelector("#dataInfo");
-  localStorages.map((el, i) => {
-    let div = document.createElement("div");
-    let id = document.createElement("p");
-    let text = document.createElement("p");
+let name = 'mehfooz';
 
-    div.className = "dataRow";
+const appendData = () => {
+  console.log(arrStorage, 'arr');
+  const dataInfo = document.querySelector('#dataInfo');
+  dataInfo.innerHTML = '';
+  arrStorage &&
+    arrStorage.forEach((el, i) => {
+      let div = document.createElement('div');
+      let id = document.createElement('p');
+      let text = document.createElement('p');
 
-    id.innerText = el.id;
-    text.innerText = el.todoText;
+      div.className = 'dataRow';
 
-    div.append(id, text);
-    dataInfo.append(div);
-  });
+      id.innerText = el.id;
+      text.innerText = el.todoText;
+
+      // here i am created btn for 'edit' & 'delete'
+      let editBtn = document.createElement('button');
+      editBtn.innerText = 'edits';
+      editBtn.classList = 'edit-btn';
+      let deleteBtn = document.createElement('button');
+      deleteBtn.innerText = 'delete';
+      deleteBtn.classList = 'delete-btn';
+
+      div.append(id, text);
+      dataInfo.append(div);
+    });
 };
