4eec13d0d6c3afeda607741a838c82b5a00be7ab
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-05-30 13:03:17 +0530

    code of the day

--- Project/crud_todos_json/index.js
+++ Project/crud_todos_json/index.js
@@ -41,15 +41,47 @@ async function appendData() {
     let div = document.createElement('div');
     let id = document.createElement('p');
     let text = document.createElement('p');
+    let input = document.createElement('input'); //? here we have to swap the text od p into input text with the current value.
 
     let editBtn = document.createElement('button');
     let deleteBtn = document.createElement('button');
 
+    let x = document.createElement('button'); //? this is cancel update btn.
+    let y = document.createElement('button'); //? this is confirm update btn.
+
     id.innerText = el.id;
     text.innerText = el.text;
 
+    text.style.display = el.isEdits ? 'none' : 'inline-block';
+
+    x.innerText = 'cancle';
+    y.innerText = 'confirm';
+
+    // this is class name
+    x.classList = 'delete-btn';
+    y.classList = 'edit-btn';
+    input.value = el.text;
+
+    input.style.display = el.isEdits ? 'block' : 'none';
+    x.style.display = el.isEdits ? 'block' : 'none';
+    y.style.display = el.isEdits ? 'block' : 'none';
+
     editBtn.innerText = 'edits';
     deleteBtn.innerText = 'delete';
+
+    editBtn.addEventListener('click', async function () {
+      try {
+        const res = await fetch(`${api}/${el.id}`, {
+          method: 'PATCH',
+          body: JSON.stringify({ isEdits: !el.isEdits }),
+          headers: {
+            'Content-type': 'application/json',
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    });
 
     deleteBtn.addEventListener('click', async function () {
       try {
@@ -64,6 +96,9 @@ async function appendData() {
         console.log(error);
       }
     });
+
+    editBtn.style.display = el.isEdits ? 'none' : 'block';
+    deleteBtn.style.display = el.isEdits ? 'none' : 'block';
 
     div.append(id, text, editBtn, deleteBtn);
     main_div.append(div);
