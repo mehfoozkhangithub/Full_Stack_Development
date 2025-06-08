fb75ccc05241344855fe32b972fafa9275a8984e
mehfoozkhangihub <khanmehfooz78603@gmail.com>
2025-06-08 17:05:46 +0530

    code add of today task

--- html_css_js/JavaScript/Basic_js/dummy.js
+++ html_css_js/JavaScript/Basic_js/dummy.js
@@ -1,10 +1,16 @@
 // 6. Password Checker
 
-let password = process.argv[2];
-let pass = "Hetuk123";
+// let password = process.argv[2];
+// let pass = "Hetuk123";
 
-while (password !== pass) {
-  console.log("Incorrect Password Please Try Again!");
-  return;
-}
-console.log("You Have Entered Correct Password: ", password);
+// while (password !== pass) {
+//   console.log("Incorrect Password Please Try Again!");
+//   return;
+// }
+// console.log("You Have Entered Correct Password: ", password);
+
+const input_1 = process.argv[2];
+const input_2 = process.argv[3];
+
+console.log(' input_1:', input_1);
+console.log(' input_2:', input_2);
