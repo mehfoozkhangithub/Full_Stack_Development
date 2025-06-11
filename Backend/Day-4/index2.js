e2dffa1f9d7f30d39b3572d9c926ee83419f01ec
mehfoozkhangihub <khanmehfooz78603@gmail.com>
2025-06-11 16:05:56 +0530

    codeof the day

--- Backend/Day-4/index2.js
+++ Backend/Day-4/index2.js
@@ -1,21 +1,23 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
+const fs = require('fs');
 
 const Port = 8000;
 
 const app = express();
 
+app.use(express.json()); // we have to use this middile-ware when we are dealing with json file or concept
+
 // crud operation...
 
-app.get("/", (req, res) => {
-  res.send("add data...");
+app.get('/', (req, res) => {
+  res.send('add data...');
 });
 
-app.post("/students", (req, res) => {
-  fs.writeFileSync("addStudents", JSON.stringify(req.body), "utf-8");
-  res.send("accepted data...");
+app.post('/students', (req, res) => {
+  fs.writeFileSync('db.json', JSON.stringify(req.body), 'utf-8');
+  res.send('accepted data...');
 });
 
 app.listen(Port, () => {
-  console.log("Port was Listing: ", Port);
+  console.log('Port was Listing: ', Port);
 });
--- Backend/Day-4/index3.js
+++ Backend/Day-4/index3.js
@@ -1,1 +1,28 @@
 // crud operation
+
+const express = require('express');
+
+const fs = require('fs');
+
+const PORT = 8500;
+
+const app = express();
+
+app.use(express.text());
+
+app.get('/showData', (req, res) => {
+  const data = fs.readFileSync('./db.json', { encoding: 'utf-8' });
+  res.send(data);
+});
+
+// # create file
+
+app.post('/createData', (req, res) => {
+  //   console.log(req.body);
+  fs.writeFileSync('student.txt', req.body, 'utf-8');
+  res.send('Gotchh your data.....');
+});
+
+app.listen(PORT, () => {
+  console.log(`Port is listing on ${PORT}`);
+});
