7df186a6cfb50c697bc5b49a4d734412c01f015c
mehfoozkhangihub <khanmehfooz78603@gmail.com>
2025-06-12 12:46:23 +0530

    code of the day

--- Backend/Day-5/SimpleMiddleware.js
+++ Backend/Day-5/SimpleMiddleware.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const port = 9800;
 
 const app = express();
@@ -7,39 +7,39 @@ const app = express();
 
 // always use on top
 app.use((req, res, next) => {
-  console.log("hello i am  from middleware..");
+  console.log('hello i am  from middleware..');
   next();
-  console.log("i am next to the middleware function");
+  console.log('i am next to the middleware function');
 });
 
 // example:2
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   if (1 + 2 == 5) {
     res.send("Bye");
   } else {
     next();
   }
+}); */
+
+app.get('/', (req, res) => {
+  console.log('hello i am  from base route..');
+  res.send('welcome');
 });
 
-app.get("/", (req, res) => {
-  console.log("hello i am  from base route..");
-  res.send("welcome");
+app.get('/contact', (req, res) => {
+  console.log('hello i am from contact..');
+  res.send('contact');
 });
 
-app.get("/contact", (req, res) => {
-  console.log("hello i am from contact..");
-  res.send("contact");
+app.get('/about', (req, res) => {
+  console.log('hello i am  from about..');
+  res.send('about');
 });
 
-app.get("/about", (req, res) => {
-  console.log("hello i am  from about..");
-  res.send("about");
-});
-
-app.get("/blogs", (req, res) => {
-  console.log("hello i am  from blogs..");
-  res.send("blogs");
+app.get('/blogs', (req, res) => {
+  console.log('hello i am  from blogs..');
+  res.send('blogs');
 });
 
 app.listen(port, () => {
