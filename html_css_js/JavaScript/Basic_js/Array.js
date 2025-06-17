0b4acf2cde31f77d69b180332fd40177ee624663
mehfoozkhangihub <khanmehfooz78603@gmail.com>
2025-06-17 16:15:28 +0530

    code of the day Array

---
+++ html_css_js/JavaScript/Basic_js/Array.js
@@ -0,0 +1,33 @@
+let arr = ['mahfooz', 'farhan', 'hetuk', 'khushi'];
+
+let arr1 = [
+  { name: 'mehfooz_khan', age: 88 },
+  { name: 'khushi', age: 188 },
+];
+
+let arr2 = [false, true, false];
+
+let arr3 = [1, 2, 3, [4, 5, [6, 7, 8], [9, 10]]];
+// console.log(' arr3:', arr3);
+
+let arr4 = [
+  [1, 2, 3],
+  [1, 2, 3],
+  [1, 2, 3],
+];
+// console.log(' arr4:', arr4);
+
+// for (let i = 1; i <= array.length; i++) {
+//   console.log(' i:', i);
+// }
+
+// array.forEach((element) => {
+//   console.log(' element:', element);
+// });
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// array.push(11, 12);
+array.pop();
+
+console.log(' array:', array);
