d8b9a50231e3696915283443ec60c50ba3f61669
mehfoozkhangihub <khanmehfooz78603@gmail.com>
2025-06-29 10:30:43 +0530

    code of the day

--- React/reactForms/src/Components/Form.jsx
+++ React/reactForms/src/Components/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { CustomForm } from "./customComponents";
 
 export const Form = () => {
--- React/reactForms/src/Components/customComponents.jsx
+++ React/reactForms/src/Components/customComponents.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // customComponents
 
 /**
- * 
- * @param {something} param0 
- * @returns 
- * 
+ *
+ * @param {something} param0
+ * @returns
+ *
  */
 
 /**
@@ -15,65 +15,80 @@ import PropTypes from "prop-types";
  * username:""
  * email:""
  * pasword:""
- * } 
+ * }
  */
 
 export const CustomForm = ({ fields, onSubmit, buttonText }) => {
-    const [formData, setFormData] = React.useState(() => {
-        // Initialize form data with empty value
-        return fields.reduce((accumulator, field) => {
-            accumulator[field.name] = "";
-            return accumulator;
-        }, {});
-    });
-    // console.log(fields, "this is field array");
-    // handle input change.
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prev) => ({
-            ...prev, [name]: value
-        }))
-    }
-    // handle form sumbission
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // console.log(formData);
-        onSubmit(formData); //pass frorm data to the parent
-    }
-    return (
-        <form onSubmit={handleSubmit}>
-            {
-                fields?.map((el) => (
-                    <div key={el.name}>
-                        <label htmlFor={el.name}>
-                            {el.label}
-                        </label>
-                        <input type={el.type}
-                            name={el.name}
-                            id={el.id}
-                            placeholder={el.placeholder}
-                            value={formData[el.name]}
-                            onChange={(e) => handleChange(e)}
-                            required={el.required}
-                        />
-                    </div>
-                ))
-            }
-            <button type="submit">{buttonText}</button>
-        </form>
-    );
+  const [formData, setFormData] = React.useState(() => {
+    // Initialize form data with empty value
+    return fields.reduce((accumulator, field) => {
+      accumulator[field.name] = '';
+      return accumulator;
+    }, {});
+  });
+
+  console.log('-> formData:', formData);
+
+  // console.log(fields, "this is field array");
+  // handle input change.
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  // handle form sumbission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(formData);
+    onSubmit(formData); //pass frorm data to the parent
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      {fields?.map((el) => (
+        <div key={el.name}>
+          <label htmlFor={el.name}>{el.label}</label>
+          <input
+            type={el.type}
+            name={el.name}
+            id={el.id}
+            placeholder={el.placeholder}
+            value={formData[el.name]}
+            onChange={(e) => handleChange(e)}
+            required={el.required}
+          />
+        </div>
+      ))}
+      <button type="submit">{buttonText}</button>
+    </form>
+  );
 };
 
 // PropTypes Validation
 
-CustomForm.propTypes = {
-    fields: PropTypes.arrayOf(PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        type: PropTypes.string.isRequired,
-        placeholder: PropTypes.string,
-        label: PropTypes.string.isRequired,
-        required: PropTypes.bool,
-    })).isRequired,
-    onSubmit: PropTypes.func.isRequired,
-    buttonText: PropTypes.string.isRequired
+/* 
+
+[
+{
+name:string,
+type:string,
+placeholder: string,
+label: string,
+required: bool,
 }
+]
+*/
+CustomForm.propTypes = {
+  fields: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      placeholder: PropTypes.string,
+      label: PropTypes.string.isRequired,
+      required: PropTypes.bool,
+    })
+  ).isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  buttonText: PropTypes.string.isRequired,
+};
