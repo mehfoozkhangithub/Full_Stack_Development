f8af142e6d2de8e9151824ee35b92420b3e3b854
mehfoozkhangihub <khanmehfooz78603@gmail.com>
2025-07-01 18:02:12 +0530

    cdoe of the day

--- html_css_js/JavaScript/Basic_js/ArrayMethod.js
+++ html_css_js/JavaScript/Basic_js/ArrayMethod.js
@@ -1,7 +1,8 @@
-let array = [1, 3, 3, 4, 5];
+let array = [1, 2, 6, 4, 5, 3];
+// console.log('-> array:', array[0]);
 
-console.log(array.findIndex((value) => value === 3));
-// console.log(array.indexOf(4));
+// console.log(array.findIndex((value) => value === 3));
+console.log(array.indexOf(5));
 
 // map --> []
 //@  filter -> [reduce the conditonal value];
@@ -11,16 +12,16 @@ let arr = [2, 3, 1, 7, 5, 99, 100, 10, 125];
 // arr.sort((a, b) => a - b);
 
 // arr.splice(4, 0, 2);
-// console.log('-> arr:', arr);
+// // console.log('-> arr:', arr);
 
 let ans = arr.slice(3, 6);
 
-// console.log('-> ans:', ans);
+// // console.log('-> ans:', ans);
 
 let arr1 = arr.join(' ');
-console.log('-> arr1:', arr1);
+// console.log('-> arr1:', arr1);
 
 let array1 = [1, 3, 3, 4, 5];
-array1.pop();
+// array1.pop();
 array1.push(6, 7, 8, 9, 10);
 console.log('-> array1:', array1);
--- html_css_js/JavaScript/Basic_js/notes.md
+++ html_css_js/JavaScript/Basic_js/notes.md
@@ -10,9 +10,8 @@ console.log("-> arrAns:", arrAns);// [true,true,true,true,true,true,fasle]
 let arrAns=arr.filter((el)=>{
     return el!=7
 })
+
 console.log("-> arrAns:", arrAns); // [1,2,3,4,5,6]
-
-
 
 let arrAns=arr.forEach(()=>{})
 console.log("-> arrAns:", arrAns);// undefined
@@ -25,7 +24,5 @@ let arrAns=arr.reduce(()=>{})//  return single value after the calculation for y
 
 // MASAI -> STRING -> ARRAY[ FUNCTION ]
 {m:1,a:2,s:1,i:1}
-
-
 
 ```
