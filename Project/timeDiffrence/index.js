a7daf75e84b513bea9af8956824224bb1360caf8
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-07-25 06:51:08 +0530

    code of the day

--- Project/timeDiffrence/index.js
+++ Project/timeDiffrence/index.js
@@ -1,3 +1,18 @@
-let name = moment().format('MMMM Do YYYY, h:mm:ss a');
+let getInTime = moment().format('h:mm ');
+console.warn('-> ~ getInTime:', getInTime);
+// let name = moment().format('MMMM Do YYYY, h:mm:ss a');
+/* 
+# here we will do if we need 24hr value
 
-document.body.innerHTML += name;
+let time12 = "7:45 PM";
+let time24 = moment(time12, "h:mm A").format("HH:mm");
+console.warn('-> ~ time24:', time24);
+
+*/
+
+const inTime = document.querySelector('#in-time');
+const outTime = document.querySelector('#out-time');
+
+inTime.value = `${getInTime}`;
+outTime.value = `08:00`
+
