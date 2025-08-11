977b17e2f641571d74c4d1c14d1d7c2d634c38f7
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-08-11 08:12:20 +0530

    code of the day

---
+++ html_css_js/JavaScript/jest_testing_js/full_concept/Api/Api.test.js
@@ -0,0 +1,34 @@
+import { apiCall } from './Api';
+
+describe('apiCall', () => {
+    beforeEach(() => {
+        global.fetch = jest.fn(); // mock fetch globally
+    });
+
+    test('should return data when fetch is successful', async () => {
+        const mockData = { message: 'Hello World' };
+
+        global.fetch.mockResolvedValue({
+            json: jest.fn().mockResolvedValue(mockData),
+        });
+
+        const result = await apiCall('https://fakestoreapi.com/products');
+        console.log('🚀 ~ result:', result);
+        expect(result).toEqual(mockData);
+        expect(global.fetch).toHaveBeenCalledWith('https://fakestoreapi.com/products');
+    });
+
+    test('should handle fetch errors', async () => {
+        const consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => { });
+
+        global.fetch.mockRejectedValue(new Error('Network error'));
+
+        const result = await apiCall('https://fakestoreapi.com/products');
+        console.log('🚀 ~ result:', result);
+        expect(result).toBeUndefined();
+        expect(consoleSpy).toHaveBeenCalledWith(new Error('Network error'));
+
+        consoleSpy.
+        ();
+    });
+});
