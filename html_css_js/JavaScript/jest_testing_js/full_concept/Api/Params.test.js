1cc9165c1d13e57502bf7106c7bc090bba93fd82
mehfoozkhangihub <khanmehfooz78603@gmail.com>
2025-09-02 17:20:09 +0530

    code of the day

--- html_css_js/JavaScript/jest_testing_js/full_concept/Api/Params.test.js
+++ html_css_js/JavaScript/jest_testing_js/full_concept/Api/Params.test.js
@@ -3,8 +3,8 @@ import { SearchParams } from "./Params";
 test("test the search params", () => {
   expect(
     SearchParams(
-      `https://jsonplaceholder.typicode.com/todos?_limit=11&_page=4`,
-      `_limit`
+      `https://jsonplaceholder.typicode.com/todos?_limit=10&_page=4`,
+      `_page`
     )
   ).toBe("10");
 });
