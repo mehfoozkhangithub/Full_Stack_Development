4d0af2f523eba68bac11fe8b26a67b57e46543dc
mehfoozkhangihub <khanmehfooz78603@gmail.com>
2025-09-08 18:23:50 +0530

    code of the day

--- React/react-into-1/src/App.js
+++ React/react-into-1/src/App.js
@@ -1,28 +1,32 @@
+import React from 'react';
 import './App.css';
 
+import { UseRef } from "./Components/useRef.jsx"
 
+export function App() {
 
-function App() {
+  let [count, setCount] = React.useState(0);
 
-  let count = 0;
+  // let count=0
 
   const handleIncre = () => {
-    count += 1;
-    document.getElementById("counter").innerText = `Counter ${count}`;
+    setCount((prev) => prev + 1);
+    // count += 1;
+    // document.getElementById("counter").innerText = `Counter ${count}`;
   }
   const handleDecre = () => {
-    count -= 1;
-    document.getElementById("counter").innerText = `Counter ${count}`;
+    setCount((prev) => prev - 1);
+    // count -= 1;
+    // document.getElementById("counter").innerText = `Counter ${count}`;
   }
 
   // hello
   return (
     <>
       <h1 id='counter'>Counter {count}</h1>
-      <button onClick={handleIncre}>+</button>
-      <button onClick={handleDecre}>-</button>
+      <button disabled={count === 10} onClick={handleIncre}>+</button>
+      <button disabled={count <= 0} onClick={handleDecre}>-</button>
+      <UseRef />
     </>
   );
 }
-
-export default App;
