098edfa6f943552857d271b4f862415072f4be45
mehfoozkhangihub <khanmehfooz78603@gmail.com>
2025-09-10 15:40:18 +0530

    code of the day

--- Project/propdrill_calculator/src/components/Profile.jsx
+++ Project/propdrill_calculator/src/components/Profile.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-
 import "./Profile.css"
 import data from "../Data/Data.json"
 
 export const Profile = () => {
-    console.log(data);
 
     return (
         <>
             {
-                data.profile.map((el) => (
-                    <main>
+                data && data.profile.map((el) => (
+                    <main key={el.name}>
                         <section className="child-1">
                             <div className="info">
                                 <h3 name="name">name: {el.name}</h3>
@@ -19,13 +16,11 @@ export const Profile = () => {
                                 <div className='platform-div'>
                                     <p name="platform">platform: </p>
                                     {
-                                        el.platform.map((els) => (
-                                            <p style={{ borderRadius: '10px', padding: "1px 10px", background: els.color, color: "#ffff" }} name="platform">{els.name}</p>
+                                        el && el.platform.map((els) => (
+                                            <p key={els.name} style={{ borderRadius: '10px', padding: "1px 10px", background: els.color, color: "#ffff" }} name="platform">{els.name}</p>
                                         ))
                                     }
                                 </div>
-
-
                             </div>
                             <div className="img-prf">
                                 <img src={el.img} alt={el.name} />
@@ -33,8 +28,8 @@ export const Profile = () => {
                         </section>
                         <section className="child-2">
                             {
-                                el.socialMedia.map((elss) => (
-                                    <div className="box-1">
+                                el && el.socialMedia.map((elss) => (
+                                    <div key={elss.rate} className="box-1">
                                         <h2>{elss.rate}</h2>
                                         <p>{elss.text1}</p>
                                         <div style={{ background: elss.color, padding: "10px 20px", borderRadius: "10px" }} className='img-ss'>
