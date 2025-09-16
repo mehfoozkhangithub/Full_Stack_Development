14cdacf644a431182d3d9057625c945ebfb274fc
mehfoozkhangihub <khanmehfooz78603@gmail.com>
2025-09-16 17:24:30 +0530

    code of the day

---
+++ Digital_Marketing/web-worker-demo/newApproch/index.js
@@ -0,0 +1,10 @@
+// worker.js
+// This runs in the background
+self.onmessage = function (event) {
+    let num = event.data;
+    let result = 0;
+    for (let i = 0; i < num; i++) {
+        result += i;
+    }
+    self.postMessage(result); // send result back to main thread
+};
