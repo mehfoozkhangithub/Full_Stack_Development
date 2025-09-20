41f168bb9a7892e7f58e3b83bb0c7c2287702cd4
mehfoozkhangihub <khanmehfooz78603@gmail.com>
2025-09-20 18:08:44 +0530

    code

--- React/useReducers/src/Components/Counters.jsx
+++ React/useReducers/src/Components/Counters.jsx
@@ -14,7 +14,7 @@ const inithislState = { count: 0 };
 
 export const Counters = () => {
   const [state, dispatch] = React.useReducer(Reducers, inithislState);
-  const [data, setData] = React.useState("");
+  const [data, setData] = React.useState(null);
   // console.log(state.count);
 
   // ! Please install evernote best extension for making notes in computer.
@@ -25,7 +25,7 @@ export const Counters = () => {
         <h1>Counter:{state.count}</h1>
         <input
           type="number"
-          onChange={(e) => setData(Number(e.target.value))}
+          onChange={(e) => setData(+e.target.value)}
         />
 
         <button onClick={() => dispatch(Increments)}>Increment</button>
@@ -40,7 +40,7 @@ export const Counters = () => {
         <button onClick={() => dispatch(incrementByValue(data ? data : 5))}>
           Increments By Value
         </button>
-        <button onClick={() => dispatch(decrementByValue(data ? data : 5))}>
+        <button disabled={state.count === 0} onClick={() => dispatch(decrementByValue(data ? data : 5))}>
           Decrements By Value
         </button>
       </div>
--- html_css_js/JavaScript/Fetch/demo/index.js
+++ html_css_js/JavaScript/Fetch/demo/index.js
@@ -1,5 +1,7 @@
-const BASE_URL = `https://jsonplaceholder.typicode.com/posts`;
+const BASE_URL = `https://official-joke-api.appspot.com/random_joke`;
+// const BASE_URL = `https://jsonplaceholder.typicode.com/posts`;
 
+`https://jsonplaceholder.typicode.com/comments`
 
 const apiCalling = async () => {
 
@@ -7,6 +9,13 @@ const apiCalling = async () => {
         let data = await fetch(BASE_URL);
         let res = await data.json();
         console.log('🚀 ~ res:', res);
+        alert(
+            `id ${res.id}
+        type ${res.type}
+        setup ${res.setup}
+        punchline ${res.punchline}
+        `
+        )
     } catch (error) {
         console.log('🚀 ~ error:', error);
     }
@@ -15,4 +24,3 @@ const apiCalling = async () => {
     /*  fetch(BASE_URL).then((res) => res.json()).then((res) => console.log(res)).catch((error) => console.log(error)).finally(() => console.log("data has been fetch")); */
 
 }
-apiCalling()
