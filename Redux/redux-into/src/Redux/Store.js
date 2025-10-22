b11e9ad1e8b5eac1701392ad8f3734e7afd56ff4
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-10-22 17:02:25 +0530

    code of the day

--- Redux/redux-into/src/Redux/Store.js
+++ Redux/redux-into/src/Redux/Store.js
@@ -2,6 +2,6 @@ import { legacy_createStore } from './../../node_modules/redux/src/createStore';
 import { reducer } from './Reducer';
 
 // useREducer(reducer,initialState)
-const store = legacy_createStore(reducer, { count: 15 })
+const store = legacy_createStore(reducer, { count: 15 });
 
 export { store };
